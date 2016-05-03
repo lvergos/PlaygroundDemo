93458b0f411e7a2dd14da466268f28cd708cd3f8
Luke Vergos <lvergos@viator.com>
2016-05-03 16:06:27 +1000

    project setup

---
+++ PlaygroundDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PlaygroundDemo
+//
+//  Created by Luke Vergos on 3/05/2016.
+//  Copyright © 2016 Luke Vergos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
